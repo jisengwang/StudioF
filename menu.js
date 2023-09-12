df06403feb3a65f80ad0f520b35fbf6194065900
Colette <hello@colettesmith.com>
2023-09-12 13:42:16 +0100

    attemped to sort menu

--- menu.js
+++ menu.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.getElementById("toggleButton");
   const content = document.getElementById("popup-menu");
+  const overlay = document.querySelector(".overlay"); // Select the overlay element
 
-  toggleButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    if (content.classList.contains("hidden")) {
+  toggleButton.addEventListener("click", function () {
+    if (overlay.classList.contains("hidden")) {
       // Show the content
-      content.classList.remove("hidden");
-      content.style.opacity = "1";
+      overlay.classList.remove("hidden");
+      overlay.style.opacity = "1";
+      content.style.opacity = "1"; // Apply opacity to popup-menu
       toggleButton.textContent = "CLOSE";
     } else {
-      // Hide the content content.style.opacity = "0";
-
-      content.classList.add("hidden");
-      // content.style.opacity = "1"; // Reset opacity for next opening
+      // Hide the content
+      overlay.classList.add("hidden");
+      overlay.style.opacity = "0"; // Apply opacity to overlay
+      content.style.opacity = "0"; // Apply opacity to popup-menu
       toggleButton.textContent = "CONTACT";
     }
   });
