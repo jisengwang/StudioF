d96bad26c2b27618d76ccdeaaecc2c169834cd2e
Colette <hello@colettesmith.com>
2023-09-12 12:54:56 +0100

    styles on form

--- menu.js
+++ menu.js
@@ -2,18 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.getElementById("toggleButton");
   const content = document.getElementById("popup-menu");
 
-  toggleButton.addEventListener("click", function () {
+  toggleButton.addEventListener("click", function (e) {
+    e.preventDefault();
     if (content.classList.contains("hidden")) {
       // Show the content
       content.classList.remove("hidden");
+      content.style.opacity = "1";
       toggleButton.textContent = "CLOSE";
     } else {
       // Hide the content content.style.opacity = "0";
-      setTimeout(function () {
-        content.classList.add("hidden");
-        content.style.opacity = "1"; // Reset opacity for next opening
-        toggleButton.textContent = "CONTACT";
-      }, 300);
+
+      content.classList.add("hidden");
+      // content.style.opacity = "1"; // Reset opacity for next opening
+      toggleButton.textContent = "CONTACT";
     }
   });
 });
