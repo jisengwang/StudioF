4996b22f8fbaaac192acecce2710312c69abb58d
Colette <hello@colettesmith.com>
2023-09-13 09:56:53 +0100

    tweaks

--- menu.js
+++ menu.js
@@ -1,32 +1,12 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const toggleButton = document.getElementById("toggleButton");
-//   const content = document.getElementById("popup-menu");
-//   const overlay = document.querySelector(".overlay"); // Select the overlay element
-
-//   toggleButton.addEventListener("click", function () {
-//     if (overlay.classList.contains("hidden")) {
-//       // Show the content
-//       overlay.classList.remove("hidden");
-//       overlay.style.opacity = "1";
-//       content.style.opacity = "1"; // Apply opacity to popup-menu
-//       toggleButton.textContent = "CLOSE";
-//     } else {
-//       // Hide the content
-//       overlay.classList.add("hidden");
-//       overlay.style.opacity = "0"; // Apply opacity to overlay
-//       content.style.opacity = "0"; // Apply opacity to popup-menu
-//       toggleButton.textContent = "CONTACT";
-//     }
-//   });
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.getElementById("toggleButton");
   const popupMenu = document.getElementById("popupMenu");
   const closeButton = document.getElementById("closeButton");
+  // const veil = document.getElementById("veil");
 
   toggleButton.addEventListener("click", function () {
     popupMenu.classList.toggle("hidden");
+    // veil.style.display = veil.style.display === "block" ? "none" : "block";
     // Update the button text based on the menu's visibility
     if (popupMenu.classList.contains("hidden")) {
       toggleButton.textContent = "CONTACT";
@@ -37,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   closeButton.addEventListener("click", function () {
     popupMenu.classList.add("hidden");
+    // veil.style.display = "none";
     toggleButton.textContent = "CONTACT"; // Reset the button text
   });
 });
